7523db0d911d2c57732fbf8f1129ecbbafb9aafd
davidgutsch <david.gutsch@banno.com>
2023-08-15 14:20:30 -0400

    add open ai

--- emacs/init.el
+++ emacs/init.el
@@ -675,6 +675,18 @@
 	("M-f" . 'copilot-accept-completion-by-word)
 	("M-<return>" . 'copilot-accept-completion-by-line)))
 
+;; Open-AI
+(use-package org-ai
+  :ensure t
+  :commands (org-ai-mode
+             org-ai-global-mode)
+  :init
+  (add-hook 'org-mode-hook #'org-ai-mode) ; enable org-ai in org-mode
+  (org-ai-global-mode) ; installs global keybindings on C-c M-a
+  :config
+  (setq org-ai-default-chat-model "gpt-4") ; if you are on the gpt-4 beta:
+  (org-ai-install-yasnippets)) ; if you are using yasnippet and want `ai` snippets
+
 (use-package nix-mode
   :ensure t
   :demand t
