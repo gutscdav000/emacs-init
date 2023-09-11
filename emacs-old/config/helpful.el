589f8016fcd968d1d4e1d5fa407fb034b507919a
davidgutsch <david.gutsch@banno.com>
2023-09-11 11:40:29 -0400

    add readme. delete pre-nix emacs config

--- emacs-old/config/helpful.el
+++
@@ -1,7 +0,0 @@
-(use-package helpful
-  :demand t)
-
-(use-package which-key
-  :demand
-  :init
-  (which-key-mode 1))
--- emacs-old/config/openapi.el
+++
@@ -1,7 +0,0 @@
-(require 'use-package)
-
-(use-package openapi-yaml-mode
-  :load-path "external-packages/openapi-yaml-mode"
-  :defer t)
-
-;;;
